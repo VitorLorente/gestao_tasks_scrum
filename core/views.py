03b13bdcd4d4e03f0838e31ac1a655bf368456de
VitorLorente <lorente.sociologia@gmail.com>
2019-10-31 11:17:27 -0300

    Filtro estabilizado e implementação da média de duração por pontos de história.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Avg
 
 from core.models import Story
 
@@ -10,8 +11,21 @@ class StoriesList(ListView):
 
     def get_queryset(self):
         points_filter = self.request.GET.get('points', None)
-        if points_filter:
+        if points_filter and int(points_filter) > 0:
             return Story.objects.filter(
                 points=points_filter
             ).order_by('creation_date')
         return Story.objects.all()
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        points_filter = self.request.GET.get('points', None)
+        data['duration_average'] = None
+        data['points'] = None
+        if points_filter and int(points_filter) > 0:
+            duration_average = Story.objects.filter(
+                points=points_filter
+            ).aggregate(Avg('duration'))['duration__avg']
+            data['duration_average'] = duration_average
+            data['points'] = points_filter
+        return data
